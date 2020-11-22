c7d8ac5bd46d51e84d5b904eb47ca162e80af0bb
Ben Paget <ben.paget@live.fr>
2020-11-22 15:58:29 +0100

    Added CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
